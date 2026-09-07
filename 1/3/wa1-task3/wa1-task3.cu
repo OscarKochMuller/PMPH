d59aea9ffc860f69152384b0212eb1b87cbe5b7b
Oscar Koch-Müller <okochmuller@gmail.com>
2026-09-07 09:34:25 +0200

    fix typo

--- 1/3/wa1-task3/wa1-task3.cu
+++ 1/3/wa1-task3/wa1-task3.cu
@@ -95,7 +95,7 @@ int main(int argc, char** argv) {
     unsigned int numblocks = (N + B - 1) / B; // number of blocks in dimension x
 
     
-    dim3 block(B, 1, 1), dim3 grid(numblocks, 1, 1); // total number of threads (numblocks*B) may overshoot N!
+    dim3 block(B, 1, 1), grid(numblocks, 1, 1); // total number of threads (numblocks*B) may overshoot N!
     
 
 
