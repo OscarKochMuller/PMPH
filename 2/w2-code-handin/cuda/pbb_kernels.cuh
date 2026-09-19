ac04893fcc14ca551b6657f5ee107b6b152b6612
Oscar Koch-Müller <okochmuller@gmail.com>
2026-09-19 11:03:51 +0200

    fix 2+3

--- 2/w2-code-handin/cuda/pbb_kernels.cuh
+++ 2/w2-code-handin/cuda/pbb_kernels.cuh
@@ -180,14 +180,14 @@ template<class OP>
 __device__ inline typename OP::RedElTp
 scanIncWarp( volatile typename OP::RedElTp* ptr, const uint32_t idx ) {
     const uint32_t lane = idx & (WARP-1);
-
-    if(lane==0) {
-        #pragma unroll
-        for(int i=1; i<WARP; i++) {
-            ptr[idx+i] = OP::apply(ptr[idx+i-1], ptr[idx+i]);
-        }
-    }
-    return OP::remVolatile(ptr[idx]);
+	#pragma unroll
+    for (uint32_t i=0; i < lgWARP; i++){
+		const uint32_t p = 1 << i;
+		if (lane >= p) {
+			ptr[idx]= OP::apply(ptr[idx-p], ptr[idx]);
+		}
+	}
+	return OP::remVolatile(ptr[idx]);
 }
 
 /**
@@ -436,7 +436,7 @@ copyFromGlb2ShrMem( const uint32_t glb_offs
 ) {
     #pragma unroll
     for(uint32_t i=0; i<CHUNK; i++) {
-        uint32_t loc_ind = threadIdx.x*CHUNK + i;
+        uint32_t loc_ind = i * blockDim.x + threadIdx.x;
         uint32_t glb_ind = glb_offs + loc_ind;
         T elm = ne;
         if(glb_ind < N) { elm = d_inp[glb_ind]; }
@@ -466,7 +466,7 @@ copyFromShr2GlbMem( const uint32_t glb_offs
 ) {
     #pragma unroll
     for (uint32_t i = 0; i < CHUNK; i++) {
-        uint32_t loc_ind = threadIdx.x * CHUNK + i;
+        uint32_t loc_ind = i * blockDim.x + threadIdx.x;
         uint32_t glb_ind = glb_offs + loc_ind;
         if (glb_ind < N) {
             T elm = const_cast<const T&>(shmem_red[loc_ind]);
